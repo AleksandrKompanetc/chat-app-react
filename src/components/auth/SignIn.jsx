fa4a4c87c2c159fded85d435855aa37e5af4ad07
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-28 19:08:00 +0300

    creating component SignIn

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, {useState} from 'react';
 
 const SignIn = () => {
-  return ()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  function logIn(e) {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password).then((user) => {
+      setError('');
+      setEmail('');
+      setPassword('');
+    }).catch((error) => {
+      console.log(error);
+      setError('Sorry, we dont find your account');
+    })
+  }
+  return (
+    <div>
+      <form>
+        <h2>Log In</h2>
+        <input 
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder='Enter your email'
+        />
+        <input 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder='Enter your email'
+        />
+        <button onClick={logIn}>Login</button>
+
+        {error ? <p style={{color: 'red'}}>{error}</p> : ''}
+      </form>
+    </div>
+  )
 }
 
 export default SignIn;
