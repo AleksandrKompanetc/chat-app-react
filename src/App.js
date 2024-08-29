ec721e74b2b0ffd098558edcefa85196ae82464a
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-29 16:34:15 +0300

    adding component Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import SignUp from './components/auth/SignUp';
-import SignIn from './components/auth/SignIn';
-import AuthDetails from './components/auth/AuthDetails';
+import Navbar from './components/Navbar';
+// import SignUp from './components/auth/SignUp';
+// import SignIn from './components/auth/SignIn';
+// import AuthDetails from './components/auth/AuthDetails';
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+}
 
 function App() {
   return (
-    <div className='App'>
-      <SignUp />
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+        <Navbar />
+      </section>
+
+
+      {/* <SignUp />
       <SignIn />
-      <AuthDetails />
+      <AuthDetails /> */}
     </div>
   );
 }
